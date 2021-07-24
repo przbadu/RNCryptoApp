a67419f47853c7d1f209f44d5d6b84e1b99e3313
przbadu <pushparaj.badu@gmail.com>
2021-07-24 22:27:17 +0545

    add Splash Screen to android app

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { CryptoDetail, Transaction } from "./screens";
+import React, { useEffect } from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen'
 
+import { CryptoDetail, Transaction } from "./screens";
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator
