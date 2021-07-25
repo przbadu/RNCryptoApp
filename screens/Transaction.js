54605fadeb32e3acd2ae9ac3423042c777b6c081
przbadu <pushparaj.badu@gmail.com>
2021-07-25 12:41:20 +0545

    Add Transaction screen

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,35 +1,107 @@
 import React from 'react';
-import {
-    StyleSheet,
-    View,
-    Text
-} from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, ScrollView } from 'react-native';
 
-const Transaction = () => {
+import {
+    HeaderBar,
+    CurrencyLabel,
+    TextButton,
+    TransactionHistory,
+} from '../components';
+import { dummyData, COLORS, SIZES, FONTS } from '../constants';
+
+const Transaction = ({ route }) => {
+    const [selectedCurrency, setSelectedCurrency] = React.useState(null);
+
+    React.useEffect(() => {
+        const { currency } = route.params;
+        setSelectedCurrency(currency);
+    }, []);
+
+    function renderTrade() {
+        return (
+            <View
+                style={{
+                    marginTop: SIZES.padding,
+                    marginHorizontal: SIZES.padding,
+                    padding: SIZES.padding,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.white,
+                    ...styles.shadow,
+                }}
+            >
+                <CurrencyLabel
+                    icon={selectedCurrency?.image}
+                    currency={selectedCurrency?.currency}
+                    code={selectedCurrency?.code}
+                />
+
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        marginBottom: SIZES.padding * 1.5,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Text style={{ ...FONTS.h2 }}>
+                        {selectedCurrency?.wallet.crypto}{' '}
+                        {selectedCurrency?.code}
+                    </Text>
+                    <Text style={{ color: COLORS.gray, ...FONTS.body2 }}>
+                        ${selectedCurrency?.wallet.value}
+                    </Text>
+                </View>
+
+                <TextButton
+                    label='Trade'
+                    onPress={() => console.log('Trade')}
+                />
+            </View>
+        );
+    }
+
+    function renderTransactionHistroy() {
+        return (
+            <TransactionHistory
+                customContainerStyles={{
+                    ...styles.shadow,
+                }}
+                history={selectedCurrency?.transactionHistory}
+            />
+        );
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Transaction</Text>
-        </View>
-    )
-}
+        <SafeAreaView style={{ flex: 1 }}>
+            <HeaderBar right={false} />
+
+            <ScrollView>
+                <View style={{ flex: 1, paddingBottom: SIZES.padding }}>
+                    {renderTrade()}
+                    {renderTransactionHistroy()}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     shadow: {
-        shadowColor: "#000",
+        shadowColor: '#000',
         shadowOffset: {
             width: 0,
             height: 4,
         },
-        shadowOpacity: 0.30,
+        shadowOpacity: 0.3,
         shadowRadius: 4.65,
 
         elevation: 8,
-    }
-})
+    },
+});
 
 export default Transaction;
